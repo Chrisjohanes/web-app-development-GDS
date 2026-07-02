f798b5311d66662d5f1a293d03228705ad816212
Chrisjohanes <95056992+Chrisjohanes@users.noreply.github.com>
2026-07-02 20:46:04 +0700

    feat: add routing, navbar, and footer components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="logo">GDS Church</div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/schedule">Schedule</Link>
+        </li>
+        <li>
+          <Link to="/sermon">Sermon</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Schedule.jsx
@@ -0,0 +1,9 @@
+function Schedule() {
+  return (
+    <div>
+      <h1>Service Schedule</h1>
+    </div>
+  );
+}
+
+export default Schedule;
