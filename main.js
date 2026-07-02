8e5ec5f3af38d0ee2b9489b98c7d9411e4fbc9cc
Chrisjohanes <95056992+Chrisjohanes@users.noreply.github.com>
2026-07-02 20:27:11 +0700

    chore: scaffold Vite + React project structure

--- main.js
+++
@@ -1,8 +0,0 @@
-async function loadComponent(id, file) {
-  const res = await fetch(`components/${file}`);
-  const html = await res.text();
-  document.getElementById(id).innerHTML = html;
-}
-
-loadComponent("header", "header.html");
-loadComponent("hero", "hero.html");
